b335e9204afb94d3e3c59728e719badce73e3667
Van Kessler <shlyftnick71@gmail.com>
2026-08-15 13:23:25 -0700

    Update TWRP configuration for fogona device

--- twrp_fogona.mk
+++ twrp_fogona.mk
@@ -5,25 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from AOSP / TWRP generic configurations
+$(call inherit-product,$(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common TWRP stuff.
+# Inherit TWRP extra build features
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from fogona device
-$(call inherit-product, device/motorola/fogona/device.mk)
-
+# Device identifier
 PRODUCT_DEVICE := fogona
 PRODUCT_NAME := twrp_fogona
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g play - 2024
-PRODUCT_MANUFACTURER := motorola
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G Play 2024
+PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="fogona-user 13 T3TF33.66-37 2be5e5 release-keys"
-
-BUILD_FINGERPRINT := motorola/fogona/fogona:13/T3TF33.66-37/2be5e5:user/release-keys
+    TARGET_DEVICE=fogona \
+    PRODUCT_NAME=fogona
